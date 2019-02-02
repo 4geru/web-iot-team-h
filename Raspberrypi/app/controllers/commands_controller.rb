fb709a7d883d416138f2232d8d9071ef84112198
Rp7rf <westhouse51@gmail.com>
2019-02-02 15:43:29 +0900

    change allow_origin

--- Raspberrypi/app/controllers/commands_controller.rb
+++ Raspberrypi/app/controllers/commands_controller.rb
@@ -1,6 +1,11 @@
 class CommandsController < ApplicationController
+  before_action :allow_origin
   def index
     @command = Command.last_one
     render :index, formats: 'json'
+  end
+
+  def allow_origin
+    headers['Access-Control-Allow-Origin'] = '*'
   end
 end
