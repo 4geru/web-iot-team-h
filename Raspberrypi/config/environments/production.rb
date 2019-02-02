476a8a5872de2deb2a70df3f1e6ce2c19e3f7b6b
Rp7rf <westhouse51@gmail.com>
2019-02-02 11:22:18 +0900

    add heroku setting

--- Raspberrypi/config/environments/production.rb
+++ Raspberrypi/config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+  # changed at 2019/02/02
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
