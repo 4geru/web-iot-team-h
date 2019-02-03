f0ce9778fa4a9ef6a45b9f0a21a91d8bbdcec94d
Rp7rf <westhouse51@gmail.com>
2019-02-03 12:42:45 +0900

    add all sensors

--- Raspberrypi/app/controllers/log_history_controller.rb
+++ Raspberrypi/app/controllers/log_history_controller.rb
@@ -1,6 +1,6 @@
 class LogHistoryController < ApplicationController
   def index
-    @logs = Log.all.limit(50)
+    @logs = Log.all.order(created_at: :desc).limit(20)
     @log_attributes = log_attributes
     @setting_logs = setting_logs
   end
@@ -8,15 +8,15 @@ class LogHistoryController < ApplicationController
   private
 
   def log_attributes
-    Log.new.attributes.keys - ['tanbo_id', 'id', 'created_at', 'updated_at', 'loged_at', 'camera', 'light', 'oto']
+    Log.new.attributes.keys - ['tanbo_id', 'id', 'created_at', 'updated_at', 'loged_at', 'camera']
   end
 
   def setting_logs
     { 
-      'shitsudo': { jp: '湿度', color: '#209BEE'},
-      # 'oto': { jp: '音' },
-      'ondo': { jp: '温度', color: '#FF5656' },
-      # 'light': { jp: '光度' }
+      'shitsudo': { jp: '湿度', color: '#209BEE', slice: 1, times: 1},
+      'oto': { jp: '音', color: '#23D160', slice: 1, times: 10},
+      'ondo': { jp: '温度', color: '#FF5656' , slice: 1, times: 1},
+      'light': { jp: '光度', color: '#FFDD56' , slice: 100, times: 1}
     }
   end
 end
